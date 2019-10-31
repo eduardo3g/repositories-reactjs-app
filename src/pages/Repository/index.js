78bb738b101f2cc4a750e14e0572da59042361b6
= <=>
2019-10-31 20:58:36 -0300

    chore: Issue filtering and pagination.

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import api from '../../services/api';
 
 import Container from '../../components/Container';
-import { Loading, Owner, IssueList } from './styles';
+import { Loading, Owner, IssueList, IssueFilter, PageActions } from './styles';
 
 export default class Repository extends Component {
   static propTypes = {
@@ -19,10 +19,18 @@ export default class Repository extends Component {
     repository: {}, // {} because it's only ONE repository
     issues: [], // [] array because one repository can have several issues
     loading: true,
+    filters: [
+      { state: 'all', label: 'Todas', active: true },
+      { state: 'open', label: 'Abertas', active: false },
+      { state: 'closed', label: 'Fechadas', active: false },
+    ],
+    filterIndex: 0,
+    page: 1,
   };
 
   async componentDidMount() {
     const { match } = this.props;
+    const { filters } = this.state;
 
     const repoName = decodeURIComponent(match.params.repository);
 
@@ -30,24 +38,54 @@ export default class Repository extends Component {
       api.get(`/repos/${repoName}`),
       api.get(`/repos/${repoName}/issues`, {
         params: { // cleaner way to send params on the URI
-          state: 'open',
+          state: filters.find(f => f.active).state,
           per_page: 5,
-        }
-      })
+        },
+      }),
     ]);
 
     this.setState({
       repository: repository.data, // '.data' is where data is stored on Axios (API calls)
       issues: issues.data,
       loading: false,
-    })
+    });
   }
 
+  loadIssues = async () => {
+    const { match } = this.props;
+    const { filters, filterIndex, page } = this.state;
+
+    const repoName = decodeURIComponent(match.params.repository);
+
+    const response = await api.get(`/repos/${repoName}/issues`, {
+      params: {
+        state: filters[filterIndex].state,
+        per_page: 5,
+        page,
+      },
+    });
+
+    this.setState({ issues: response.data });
+  }
+
+  handleFilterClick = async filterIndex => {
+    await this.setState({ filterIndex });
+    this.loadIssues();
+  };
+
+  handlePage = async action => {
+    const { page } = this.state;
+    await this.setState({
+      page: action === 'back' ? page - 1 : page + 1,
+    });
+    this.loadIssues();
+  };
+
   render() {
-    const { repository, issues, loading } = this.state;
+    const { repository, loading, issues, filters, filterIndex, page } = this.state;
 
     if (loading) {
-      return <Loading>Carregando</Loading>
+      return <Loading>Carregando</Loading>;
     }
 
     return (
@@ -60,6 +98,15 @@ export default class Repository extends Component {
         </Owner>
 
         <IssueList>
+        <IssueFilter active={filterIndex}>
+          {filters.map((filter, index) => (
+            <button
+              type="button"
+              key={filter.label}
+              onClick={() => this.handleFilterClick(index)}>{filter.label}</button>
+          ))}
+        </IssueFilter>
+
           {issues.map(issue => (
             <li key={String(issue.id)}>
               <img src={issue.user.avatar_url} alt={issue.user.login} />
@@ -75,7 +122,20 @@ export default class Repository extends Component {
             </li>
           ))}
         </IssueList>
+        <PageActions>
+          <button
+            type="button"
+            disabled={page < 2}
+            onClick={() => this.handlePage('back')}
+            >
+              Anterior
+          </button>
+          <span>Página {page}</span>
+          <button type="button" onClick={() => this.handlePage('next')}>
+            Próximo
+          </button>
+        </PageActions>
       </Container>
     );
-  };
+  }
 }
